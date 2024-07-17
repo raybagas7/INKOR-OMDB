0e91649d13ba1eb7477584d9cacaba34838a56c7
raybagas7 <raysam.gas@gmail.com>
2024-07-17 17:21:45 +0700

    feat(scss setup): add base styles to import
        - add _breakpoints configuration for breakpoints dom, it needed for
        responsiveness
        - add _colors to make text, and bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <p>aaa</p>
+        {children}
+      </body>
     </html>
   );
 }
