882a16d1580741112aeaead730e03b30a3a7fba2
raybagas7 <raysam.gas@gmail.com>
2024-07-17 18:50:27 +0700

    feat(navigation): styling mobile navigation
        - add style to mobile view navigation
        - INFORMATION: the project start with mobile first approach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import styles from "./page.module.scss";
 import agent from "@/libs/agent";
 
 export default function Home() {
-  const [movies, setMovies] = useState<any>([]);
-  useEffect(() => {
-    agent.Movie.list({
-      page: 1,
-      type: "movie",
-      s: "marvel",
-    })
-      .then((movies) => setMovies(movies))
-      .catch((err) => console.log(err));
-  }, []);
-  console.log(movies);
+  // const [movies, setMovies] = useState<any>([]);
+  // useEffect(() => {
+  //   agent.Movie.list({
+  //     page: 1,
+  //     type: "movie",
+  //     s: "marvel",
+  //   })
+  //     .then((movies) => setMovies(movies))
+  //     .catch((err) => console.log(err));
+  // }, []);
+  // console.log(movies);
   return (
     <main className={styles.main_container}>
       <div>TEST</div>
--- src/components/Navigation/TopNavigation/TopNavigation.tsx
+++ src/components/Navigation/TopNavigation/TopNavigation.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./TopNavigation.module.scss";
+import { IoPersonCircle } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BiSearch } from "react-icons/bi";
 
 const TopNavigation = () => {
   return (
     <div className={styles.top_nav_container}>
-      <div>Inkor</div>
+      <GiHamburgerMenu className={styles.icon} />
+      <div>Inkor Movie</div>
+      <div className={styles.right_box}>
+        <BiSearch className={styles.icon} />
+        <IoPersonCircle className={styles.icon} />
+      </div>
     </div>
   );
 };
