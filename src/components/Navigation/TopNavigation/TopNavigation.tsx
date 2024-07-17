d15edf80350e7903f484db4c18628147afb79b66
raybagas7 <raysam.gas@gmail.com>
2024-07-17 18:37:31 +0700

    chore(add dependecy): add react-icon to the deps
        - add react-icon for simple icon to the project

--- src/components/Navigation/TopNavigation/TopNavigation.tsx
+++ src/components/Navigation/TopNavigation/TopNavigation.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./TopNavigation.module.scss";
 
 const TopNavigation = () => {
-  return <div className={styles.top_nav_container}>TopNavigation</div>;
+  return (
+    <div className={styles.top_nav_container}>
+      <div>Inkor</div>
+    </div>
+  );
 };
 
 export default TopNavigation;
