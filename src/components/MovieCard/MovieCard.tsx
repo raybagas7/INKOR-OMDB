dade1c5f13d22e7a3bb61b64cf1580d69827fddc
raybagas7 <raysam.gas@gmail.com>
2024-07-17 23:19:40 +0700

    feat(animation): show information with animation
        - use framer-motion to animate information about the movie poster
        - trigger the animation when poster hovered

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,21 +1,48 @@
 import React from "react";
 import style from "./MovieCard.module.scss";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { LiaImdb } from "react-icons/lia";
 
 const MovieCard = ({ Title, Year, imdbID, Type, Poster }: Movies) => {
   console.log(Year, imdbID, Type);
 
   return (
-    <div className={style.movie_card}>
-      <Image
-        style={{ objectFit: "cover", width: "100%", height: "100%" }}
-        alt={Title}
-        src={Poster}
-        height={300}
-        width={200}
-        priority
-      />
-    </div>
+    <motion.div
+      className={style.movie_card_container}
+      whileTap={{ scale: 1 }}
+      whileHover={{ scale: 1.1 }}
+    >
+      <motion.div className={style.movie_card}>
+        <Image
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          alt={Title}
+          src={Poster}
+          height={300}
+          width={200}
+          priority
+        />
+      </motion.div>
+      <motion.div
+        className={style.movie_information_container}
+        whileHover={{ width: "auto", opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className={style.movie_information_box}>
+          <div>
+            <h5>{Title}</h5>
+            <div className={style.movie_sub_information}>
+              <p>{Year}</p>
+              <p>{Type}</p>
+            </div>
+          </div>
+          <div className={style.movie_imdb}>
+            <LiaImdb style={{ fontSize: "24px" }} />
+            <p>{imdbID}</p>
+          </div>
+        </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
